c114077e08f6d2af99f57eee4e21a9a92cf26b35
Evan Rubinton <evanrubinton@gmail.com>
2017-02-19 12:24:48 -0500

    add user_id & indexes to entities table, indexes to entity comments table

--- migrations/sqls/20170219142655-add-entities-up.sql
+++ migrations/sqls/20170219142655-add-entities-up.sql
@@ -1,4 +1,7 @@
 CREATE TABLE entities (
   id serial PRIMARY KEY,
+  user_id integer REFERENCES users NOT NULL,
   created_at timestamp DEFAULT now()
 );
+
+CREATE INDEX entities_user_id_index ON entities (user_id);
--- migrations/sqls/20170219144132-add-entity-comments-up.sql
+++ migrations/sqls/20170219144132-add-entity-comments-up.sql
@@ -5,3 +5,6 @@ CREATE TABLE entity_comments (
   user_id integer REFERENCES users NOT NULL,
   created_at timestamp DEFAULT now()
 );
+
+CREATE INDEX entity_comments_user_id_index ON entity_comments (user_id);
+CREATE INDEX entity_comments_entity_id_index ON entity_comments (entity_id);
