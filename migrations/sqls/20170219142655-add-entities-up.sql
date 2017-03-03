6274de28646e47b5579ec30df3ef9eff758b6e46
Evan Rubinton <evanrubinton@gmail.com>
2017-03-03 13:00:07 -0500

    remove user id from entities table

--- migrations/sqls/20170219142655-add-entities-up.sql
+++ migrations/sqls/20170219142655-add-entities-up.sql
@@ -1,7 +1,6 @@
 CREATE TABLE entities (
   id serial PRIMARY KEY,
-  user_id integer REFERENCES users NOT NULL,
   created_at timestamp DEFAULT now()
 );
 
-CREATE INDEX entities_user_id_index ON entities (user_id);
+/* CREATE INDEX entities_user_id_index ON entities (user_id); */
