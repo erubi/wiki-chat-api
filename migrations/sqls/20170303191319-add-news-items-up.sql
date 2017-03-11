4a2d7ff11ef6c1b3a603a16df06881789c67bb54
Evan Rubinton <evanrubinton@gmail.com>
2017-03-11 11:28:18 -0500

    update news item schema

--- migrations/sqls/20170303191319-add-news-items-up.sql
+++ migrations/sqls/20170303191319-add-news-items-up.sql
@@ -2,9 +2,12 @@ CREATE TABLE news_items (
   id integer REFERENCES entities ON DELETE CASCADE PRIMARY KEY,
   user_id integer REFERENCES users,
   news_source_id integer REFERENCES news_sources (id),
-  url varchar(100) NOT NULL,
-  header text NOT NULL,
-  body text
+  url varchar(100) UNIQUE NOT NULL,
+  author varchar(100),
+  publisher varchar(100),
+  published_at timestamp,
+  archive_url varchar(100) UNIQUE,
+  archived_at timestamp
 );
 
 CREATE INDEX news_items_user_id_index ON news_items (user_id);
