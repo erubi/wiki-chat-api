4e97caa550430cdcc557e260eccee869a007eadd
Evan Rubinton <evanrubinton@gmail.com>
2017-03-18 17:40:36 -0400

    update entity votes migration

--- migrations/sqls/20170308180021-add-entity-votes-up.sql
+++ migrations/sqls/20170308180021-add-entity-votes-up.sql
@@ -1,6 +1,10 @@
 CREATE TABLE entity_votes (
   entity_id integer REFERENCES entities ON DELETE CASCADE,
   user_id integer REFERENCES users ON DELETE CASCADE,
+  PRIMARY KEY (entity_id, user_id),
   vote integer DEFAULT 0 NOT NULL,
   created_at timestamp DEFAULT now()
 );
+
+CREATE INDEX entity_votes_user_id ON entity_votes (user_id);
+CREATE INDEX entity_votes_entity_id ON entity_votes (entity_id);
