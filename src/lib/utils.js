b92e928ea87d12bc09d972d0d7179a5e7bc68853
Evan Rubinton <evanrubinton@gmail.com>
2017-03-31 06:41:58 -0400

    add parsing of objects to base64 helpers in utils

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,7 +1,21 @@
-const toBase64 = str => new Buffer(str).toString('base64');
-const fromBase64 = str => new Buffer(str, 'base64').toString('ascii');
+// const toBase64 = str => new Buffer(str).toString('base64');
+// const fromBase64 = str => new Buffer(str, 'base64').toString('ascii');
+const toBase64 = (obj) => {
+  if (typeof obj === 'object') return new Buffer(JSON.stringify(obj)).toString('base64');
+  return new Buffer(obj).toString('base64');
+};
+
+const fromBase64 = (obj) => {
+  const res = new Buffer(obj, 'base64').toString('ascii');
+  try {
+    return JSON.parse(res);
+  } catch (e) {
+    return res;
+  }
+};
 
 module.exports = {
   toBase64,
   fromBase64,
 };
+
